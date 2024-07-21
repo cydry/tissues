f1c16ae858d5773dec7b46136d7a33f87398aeff
Yuzo Saita <yuzo.saita@gmail.com>
2024-07-21 16:33:06 +0900

    Add option to use a file for creating a issue

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,11 +5,14 @@ require_relative './issues.rb'
 module CLI
   def self.run
     opt = OptionParser.new do |opt|
-      opt.banner = "Usage: tissues <user> <project> [-k <credential>] [-d <data>]"
+      opt.banner = "Usage: tissues <user> <project> [-k <credential>] [-F <file> | -d <data>]"
     end
     enabled = {}
     opt.on('-k CRED') {|cred| enabled[:cred] = cred}
     opt.on('-d DATA') {|data| enabled[:data] = data}
+    opt.on('-F FILE') do |file|
+      enabled[:data] = parse_file(file)
+    end
     opt.parse!(ARGV)
 
     exit(false) if ARGV.size != 2
@@ -18,7 +21,7 @@ module CLI
 
     ghi = GitHubIssues.new(user, project)
 
-    if enabled.has_key? :data
+    if enabled.has_key?(:data)
       exit(false) unless enabled.has_key? :cred
       ghi.credential = enabled[:cred]
       ghi.data = enabled[:data]
@@ -27,5 +30,10 @@ module CLI
       ghi.fetch
       puts ghi
     end
+  end
+
+  def self.parse_file(filename)
+    data = File.read(filename).split("\n\n\n", 2)
+    JSON.generate({"title" => data[0],"body" => data[1]})
   end
 end
